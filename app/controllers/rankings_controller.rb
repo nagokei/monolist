005236b8d086faf1b69fe90770d6741a73afddc7
nagokei <nagokei@gmail.com>
2016-03-02 13:55:32 +0000

    halt

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,7 +1,22 @@
 class RankingsController < ApplicationController
     
   def want
+    @wants = Want.group(:item_id).order('count_id desc').limit(10).count(:id) 
+    ids = @wants.keys
+    items = Item.find(ids)
+    @items = items.sort_by{|item| @wants.index(item.id)}
     
+    # rank = 0
+    # current_count = Float::INFINITY
+    # @items.each do | item |
+    #   item.name
+    #   count = @wants[item.id]
+    #   if current_count > count
+    #     current_count = count
+    #     rank += 1
+    #   end
+    #   rank
+    # end
   end
   
   def have
